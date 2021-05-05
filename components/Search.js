a494055394a11f6b0b9719c3e822dacc5284ad5f
Chris <chris@databoxdigital.com>
2021-05-05 00:10:02 -0500

    styles list and adds app container

--- components/Search.js
+++ components/Search.js
@@ -17,7 +17,7 @@ export default function Search({handleSearch, currentSearch}) {
   return (
     <div>
       <form
-        className="mt-4 sm:flex sm:max-w-md lg:mt-0"
+        className="mt-4"
         onSubmit={handleSubmit}
       >
         <div>
@@ -25,15 +25,15 @@ export default function Search({handleSearch, currentSearch}) {
             type="text"
             name="searchTerm"
             id="searchTerm"
-            className="shadow-sm py-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+            className="shadow-sm py-3 border-2 text-center focus:border-indigo-700 min-w-full block sm:text-sm border-indigo-500 rounded-t-md"
             placeholder="Search for your favorite movie"
             onChange={handleChange}
           />
         </div>
-        <div className="rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+        <div className="rounded-b-md mt-0 sm:flex-shrink-0">
           <button
             type="submit"
-            className="w-full bg-indigo-500 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+            className="w-full bg-indigo-500 border border-transparent rounded-b-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
           >
             Search
           </button>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import useSearch from "../lib/use-search";
 import Search from "../components/Search";
-import ResultList from "../components/ResultList"
+import ResultList from "../components/ResultList";
 const searchInit = {
   term: "",
   shouldFetch: false,
@@ -13,9 +13,15 @@ export default function Home() {
   const { data, error } = useSearch(searchParams);
 
   return (
-    <>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto">
       <Search handleSearch={setSearchParams} currentSearch={searchParams} />
-      <ResultList data={data} error={error}/>
-    </>
-  )
+      <ResultList
+        data={data}
+        error={error}
+        isFetching={searchParams.shouldFetch}
+      />
+      </div>
+    </div>
+  );
 }
