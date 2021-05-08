6f768f2f74860c0efc3b618ba3cf5bd3b98e0b47
Chris <chris@databoxdigital.com>
2021-05-07 21:31:13 -0500

    moves api to https

--- lib/const.ts
+++ lib/const.ts
@@ -1,4 +1,4 @@
 // useSearch
-export const API_DOMAIN = "http://www.omdbapi.com";
+export const API_DOMAIN = "https://www.omdbapi.com";
 export const API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY;
 export const API_URL = `${API_DOMAIN}/?apikey=${API_KEY}`;
