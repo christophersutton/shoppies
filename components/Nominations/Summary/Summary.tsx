2b2043108a21cd6af0cb074c70a0355b5bfce49f
Chris <chris@databoxdigital.com>
2021-05-09 22:42:50 -0500

    adds chevron to summary bar for affordability

--- components/Nominations/Summary/Summary.tsx
+++ components/Nominations/Summary/Summary.tsx
@@ -2,6 +2,7 @@ import { Transition } from '@headlessui/react';
 import NominationBadge from './Badge';
 import ReviewButton from './ReviewButton'
 import { NominationsProps } from '../NominationsWrapper'
+import { ChevronUpIcon } from '@heroicons/react/solid'
 
 export default function Summary({ state, toggle, nominations }: NominationsProps) {
 
@@ -16,12 +17,16 @@ export default function Summary({ state, toggle, nominations }: NominationsProps
                 leave="transform ease-in duration-200"
                 leaveTo="translate-y-off"
             >
+
                 <div className={`text-white group ${state.summaryClasses}`} onClick={(() => toggle())}>
-                    <div className="max-w-3xl mx-auto px-2 h-auto py-3 flex justify-between items-center">
+                    <div className="w-full flex justify-center items-end">
+                        {nominations.length > 0 ? <ChevronUpIcon className="h-8 w-8" /> : '' }
+                    </div>
+                    <div className="max-w-3xl mx-auto px-2 h-auto pb-4 -m-t-4 flex justify-between items-center">
 
                         <div className="flex flex-grow flex-col sm:flex-row sm:items-center">
 
-                            <p className="text-md sm:text-xl sm:my-5 underline"> {state.text} </p>
+                            <p className="text-md sm:text-xl sm:my-4"> {state.text} </p>
                             <div className="flex">
                                 {nominations.map(nomination => <NominationBadge key={nomination.imdbID} />)}
                             </div>
